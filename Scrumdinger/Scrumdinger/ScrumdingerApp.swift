776612f45bea23c8e382f82fdbeac530bab531e4
Thanh Ha <zxiiro@gmail.com>
2026-01-04 19:41:08 -0500

    Chapter 1 SwiftUI essentials
    Signed-off-by: Thanh Ha <zxiiro@gmail.com>

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Thanh Ha on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
